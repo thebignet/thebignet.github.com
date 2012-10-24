ff2d647440a048eb54ac2f438df76b533bcfeeef
Jean Detoeuf <thebignet@gmail.com>
2012-10-24 20:26:50 +0200

    Twitter bootstrap

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).scroll(function(){
+  // If has not activated (has no attribute "data-top"
+  if (!$('.subnav').attr('data-top')) {
+    // If already fixed, then do nothing
+    if ($('.subnav').hasClass('subnav-fixed')) return;
+    // Remember top position
+    var offset = $('.subnav').offset()
+    $('.subnav').attr('data-top', offset.top);
+  }
+
+  if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
+    $('.subnav').addClass('subnav-fixed');
+  else
+    $('.subnav').removeClass('subnav-fixed');
+});
+
+$(function() {
+  var twitterApiUrl = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=thebignet&count=5&callback=?";
+  $.getJSON(twitterApiUrl, function(data) {
+    $(".tweets").html(_.template($("#twitter-template").html(), {data: data}));
+  });
+
+});
