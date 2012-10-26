476c8ca47ad123e0510b72abcdaafdee1242cb9f
Jean Detoeuf <thebignet@gmail.com>
2012-10-26 08:29:14 +0200

    Alimentation, images et fil github

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ $(document).scroll(function(){
     $('.subnav').removeClass('subnav-fixed');
 });
 
+function isPushEvent(event){
+  return ("PushEvent" == event.type);
+}
+
 $(function() {
   var twitterApiUrl = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=thebignet&count=5&callback=?";
   $.getJSON(twitterApiUrl, function(data) {
     $(".tweets").html(_.template($("#twitter-template").html(), {data: data}));
+  });
+
+  var githubApiUrl = "https://github.com/thebignet.json?callback=?";
+  $.getJSON(githubApiUrl, function(data) {
+    var filtered = data.filter(isPushEvent);
+    filtered.length=5;
+    $(".github").html(_.template($("#github-template").html(), {data: filtered}));
   });
 
 });
